26cd9ce045eedcfb71064f871eaf3dc8c4e46af9
Blaine Heffron <baheffron@gmail.com>
2024-11-06 14:44:55 -0500

    copy over files

---
+++ examples/soroban/cross_contract/contract_a/src/lib.rs
@@ -0,0 +1,15 @@
+#![no_std]
+
+use loam_sdk::soroban_sdk::{self, contract, contractimpl};
+
+#[contract]
+pub struct ContractA;
+
+#[contractimpl]
+impl ContractA {
+    pub fn add(x: u32, y: u32) -> u32 {
+        x.checked_add(y).expect("no overflow")
+    }
+}
+
+smartdeploy_sdk::core_riff!();
