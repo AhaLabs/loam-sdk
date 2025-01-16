14b74c310a29f6211d5ead45dfbbde8ef108dd49
Willem Wyndham <willem@ahalabs.dev>
2025-01-16 13:24:47 -0500

    feat: add asset contract

---
+++ test/asset/src/lib.rs
@@ -0,0 +1,38 @@
+#![no_std]
+use loam_sdk::{loamstorage, soroban_sdk::{self, contracttype, env, Address, InstanceStore, IntoVal, Lazy, LoamKey, PersistentMap, String, Val}};
+
+
+
+mod types;
+use types::{Txn, Allowance};
+
+#[loamstorage]
+#[derive(Default)]
+pub struct Token {
+    /// Name of the token
+    name: InstanceStore<String>,
+    /// Mapping of account addresses to their token balances
+    balances: PersistentMap<Address, i128>,
+    /// Mapping of transactions to their associated allowances
+    allowances: PersistentMap<Txn, Allowance>,
+    /// Mapping of addresses to their authorization status
+    authorized: PersistentMap<Address, bool>,
+    /// Symbol of the token
+    symbol: InstanceStore<String>,
+    /// Number of decimal places for token amounts
+    decimals: InstanceStore<u32>,
+    
+}
+
+impl Token {
+    pub fn init(
+        name: String,
+        symbol: String,
+        decimals: u32,
+    ) {
+        let mut token = Token::default();
+        token.name.set(&name);
+        token.symbol.set(&symbol);
+        token.decimals.set(&decimals);
+    }
+}
