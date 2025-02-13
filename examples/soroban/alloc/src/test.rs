904a96bc8ec3a95587b85ffe9143595d2f3286be
Willem Wyndham <willem@ahalabs.dev>
2025-02-13 14:36:05 -0500

    fix: alloc

--- examples/soroban/alloc/src/test.rs
+++ examples/soroban/alloc/src/test.rs
@@ -8,7 +8,7 @@ extern crate std;
 #[test]
 fn test() {
     let env = Env::default();
-    let contract_id = env.register_contract(None, SorobanContract__);
+    let contract_id = env.register(SorobanContract__, ());
     let client = SorobanContract__Client::new(&env, &contract_id);
     assert_eq!(client.sum(&1), 0);
     assert_eq!(client.sum(&2), 1);
